20e1c33f5f4554bdc35f7587a87f620c4c92476e
nisanga <nisanga1995511@gmail.com>
2022-10-22 09:39:59 +0530

    Serverless Project

---
+++ slsProject1/src/updatetodo.js
@@ -0,0 +1,37 @@
+const{v4} =  require("uuid")
+const AWS =  require("aws-sdk")
+
+
+const updatetodo = async (event) => {
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const { completed }=JSON.parse(event.body);
+const{ id } = event.pathParameters
+
+// const dynamoDB = aws.dynamoDB.DocumentClient();
+await dynamoDB.update({
+
+TableName:"TodoTable",
+Key:{ id },
+UpdateExpression: 'set completed = :completed',
+ExpressionAttributeValues: {
+
+    ':completed': completed
+},
+ReturnValues: "ALL_NEW"
+}).promise()
+
+return {
+ statusCode: 200,
+ body: JSON.stringify({
+
+    msg:"Todo Updated"
+
+ }),
+};
+};
+
+module.exports = {
+
+handler:updatetodo
+}
